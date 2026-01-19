9a14eaf5e94d3913138d651113688e2872624c4f
Danilo alves lopes <daniloalveslopesdev2023@gmail.com>
2026-01-18 22:59:39 -0300

    mudando para api

--- resources/views/livewire/components/error.blade.php
+++
@@ -1,1 +0,0 @@
-<span class="text-red-500 font-semibold bg-red-200 p-1.5 rounded">{{ $message }}</span>
--- resources/views/livewire/show-secretary.blade.php
+++
@@ -1,5 +0,0 @@
-<div class="container mx-auto p-2 bg-slate-100 rounded-md">
-    <h2 class="title-lg">Nome: {{ $secretary->name }}</h2>
-    <span>Data de criação: {{ date('d/m/y H:i', strtotime($secretary->created_at)) }}</span>
-{{--    {{ $secretary->created_at }}--}}
-</div>
